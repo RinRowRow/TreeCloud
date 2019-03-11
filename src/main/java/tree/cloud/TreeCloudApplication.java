c474b7d9f6906e3791d71a7f125760214bb06577
Rinat <mrsalatikus@gmail.com>
2019-03-11 17:41:59 +0300

    Initial commit

---
+++ src/main/java/tree/cloud/TreeCloudApplication.java
@@ -0,0 +1,13 @@
+package tree.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeCloudApplication.class, args);
+    }
+
+}
